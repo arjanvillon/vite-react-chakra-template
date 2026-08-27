5226560834f9cbe817d3b960d229106d10a11f62
arjanvillon <heaven.arjan@gmail.com>
2026-08-27 12:22:25 +0800

    feat: modify app

--- vite.config.ts
+++ vite.config.ts
@@ -5,31 +5,8 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import checker from 'vite-plugin-checker';
-import type { VitePWAOptions } from 'vite-plugin-pwa';
-import { VitePWA } from 'vite-plugin-pwa';
 import type { PluginOption } from 'vite-plus';
 import { defineConfig, lazyPlugins, loadEnv } from 'vite-plus';
-
-const pwaOptions: Partial<VitePWAOptions> = {
-  // TODO: enable if you want to enable PWA service worker
-  disable: true,
-  manifest: {
-    background_color: '#FFFFFF',
-    dir: 'ltr',
-    display: 'standalone',
-    lang: 'en',
-    name: 'Vite React App Template',
-    prefer_related_applications: false,
-    short_name: 'vite-react-chakra-starter',
-    start_url: '/',
-    theme_color: '#FFFFFF',
-  },
-  pwaAssets: {
-    config: true,
-    disabled: false,
-  },
-  registerType: 'autoUpdate',
-};
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -76,7 +53,6 @@ export default defineConfig(({ mode }) => {
             }),
           ]),
       visualizer({ template: 'sunburst' }) as unknown as PluginOption,
-      VitePWA(pwaOptions),
     ]),
     resolve: {
       tsconfigPaths: true,
